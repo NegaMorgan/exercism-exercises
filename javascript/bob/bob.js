b220aa17a0429706867e4f39554509760a7ee33a
Morgan Evans <morganrevans@gmail.com>
2014-05-19 14:31:44 -0400

    Refactored javascript bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,21 +1,13 @@
-var Bob = function(){
+function Bob(){
   this.hey = function(input){
     var whitespace =  !!input.match(/^(\s*)$/);
     var all_caps   =  input.toUpperCase() === input && !!input.match(/[A-Z]+/);
     var a_question =  !!input.match(/.+\?$/);
 
-    if(whitespace){
-      return "Fine. Be that way!";
-    } 
-    else if(all_caps){
-      return "Woah, chill out!";
-    } 
-    else if(a_question){
-      return "Sure.";
-    } 
-    else {
-      return "Whatever.";
-    }
+    if(whitespace){ return "Fine. Be that way!"; } 
+    else if(all_caps){ return "Woah, chill out!"; } 
+    else if(a_question){ return "Sure."; } 
+    else { return "Whatever."; }
   }
 }
 module.exports = Bob;

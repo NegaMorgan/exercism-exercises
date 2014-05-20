151a3c0145a2183ed3faaa3fe8b1120753a1310e
Morgan Evans <morganrevans@gmail.com>
2014-05-19 23:08:33 -0400

    Decompose conditional pattern on bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,12 +1,18 @@
+function isBlank(input){
+  return !!input.match(/^(\s*)$/);
+}
+function isAllCaps(input){
+  return input.toUpperCase() === input && !!input.match(/[A-Z]+/);
+}
+function isAQuestion(input){
+  return !!input.match(/.+\?$/);
+}
+
 function Bob(){
   this.hey = function(input){
-    var whitespace =  !!input.match(/^(\s*)$/);
-    var all_caps   =  input.toUpperCase() === input && !!input.match(/[A-Z]+/);
-    var a_question =  !!input.match(/.+\?$/);
-
-    if(whitespace){ return "Fine. Be that way!"; } 
-    else if(all_caps){ return "Woah, chill out!"; } 
-    else if(a_question){ return "Sure."; } 
+    if(isBlank(input)){ return "Fine. Be that way!"; } 
+    else if(isAllCaps(input)){ return "Woah, chill out!"; } 
+    else if(isAQuestion(input)){ return "Sure."; } 
     else { return "Whatever."; }
   }
 }

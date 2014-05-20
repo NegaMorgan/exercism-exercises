a85cc4bc58f6d6c7e2aab627075febe9e29e269f
Morgan Evans <morganrevans@gmail.com>
2014-05-19 23:58:28 -0400

    Refactored

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,13 +1,11 @@
 class Year
   def self.leap?(year)
-    return true if year.divisible_by?(4) && !year.divisible_by?(100)
-    return true if year.divisible_by?(4) && year.divisible_by?(100) && year.divisible_by?(400)
-    return false
+    (year.divisible_by?(4) && !year.divisible_by?(100)) || year.divisible_by?(4, 100, 400)
   end
 end
 
 class Integer
-  def divisible_by?(n)
-    self % n == 0
+  def divisible_by?(*numbers)
+    numbers.all? { |n| self % n == 0 }
   end
 end

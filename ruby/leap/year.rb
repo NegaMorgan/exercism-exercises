5bab6d1e9575ea6abcbc457682011c2dfabcc17d
Morgan Evans <morganrevans@gmail.com>
2014-05-19 22:36:05 -0400

    DRY leap/year.rb

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,19 +1,13 @@
 class Year
   def self.leap?(year)
-    return true if year.divisible_by_4? && !year.divisible_by_100?
-    return true if year.divisible_by_4? && year.divisible_by_100? && year.divisible_by_400?
+    return true if year.divisible_by?(4) && !year.divisible_by?(100)
+    return true if year.divisible_by?(4) && year.divisible_by?(100) && year.divisible_by?(400)
     return false
   end
 end
 
 class Integer
-  def divisible_by_4?
-    self % 4 == 0
-  end
-  def divisible_by_100?
-    self % 100 == 0
-  end
-  def divisible_by_400?
-    self % 400 == 0
+  def divisible_by?(n)
+    self % n == 0
   end
 end

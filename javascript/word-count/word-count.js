1738aa18b9d8b450de5df2c35e2e2e5ad115c4f0
Morgan Evans <morganrevans@gmail.com>
2014-05-22 11:50:51 -0400

    word-count WIP

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,8 +1,12 @@
 function words(string){
   var words = string.split(" ");
   var count = {};
-  words.forEach(function(word){
+  words.forEach(function(w){
+    var word = w.replace(/[^\w]/, "").toLowerCase();
+    console.log("word is: "+word);
     word in count ? count[word] += 1 : count[word] = 1;
   });
   return count;
 }
+
+module.exports = words;
